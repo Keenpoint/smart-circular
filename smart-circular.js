7f986dfa07b51eaf97a1928908455699fcb03c4f
Danilo Augusto <daniloaugusto.ita16@gmail.com>
2015-08-11 23:30:13 +0200

    Not specifying array/nested JSON anymore: treated as other cases Dropped "continue" commands Customizer is called just once each time (stocked in variable to avoid undesirable behaviour) Using lodash to simplify when possible editObject is called in only one part of the project. The conditions are now defining the replacer (of name "nextInsertion") necessary to this function JSON.stringify and JSON.parse are no longer used Use of prototype to define "object" functions Elementary case is not specified with a special function (it's the else case) General reformatting (camel case, etc.)

--- smart-circular.js
+++ smart-circular.js
@@ -3,7 +3,7 @@ var _ = require("lodash");
 /* Used to store the path until the JSON element concerned. This way the JSON source will be modifiable when we
  take an element from the queue. "result" is the JSON being constructed, the JSON to be returned with circular references
  transformed */
-var ObjectEditor = function(result, path) {//todo: why makePathName not in here?
+var ObjectEditor = function(result, path) {
     this.result = result;
     this.path = path;
 };
@@ -12,7 +12,7 @@ var ObjectEditor = function(result, path) {//todo: why makePathName not in here?
 ObjectEditor.prototype.editObject = function(replacer) {
 
     //The first call to this function (from the JSON root) shouldn't modify our result
-    if(this.path === "") return;
+    if(this.path.length === 0) return;
 
     //We construct the steps necessary to get to the concerned JSON from the root
     var arrayOfSteps = this.path
@@ -22,23 +22,11 @@ ObjectEditor.prototype.editObject = function(replacer) {
     //Recursive function that finds and replaces the JSON of path
     (function auxFindElementToReplace(pointerResult, array, replacer) {
 
-        //We parse the next step, so we can use pointerResult[nextStep]
         var nextStep = array[0];
 
         //When we arrive to the JSON of path
         if(array.length === 1) {
-            //pointerResult[nextStep] is array not yet instantiated
-            if(pointerResult[nextStep] === undefined && replacer === "partialArray") {
-                pointerResult[nextStep] = [];
-            }
-            //pointerResult[nextStep] is composed JSON not yet instantiated
-            else if(pointerResult[nextStep] === undefined && replacer === "nestedJSON") {
-                pointerResult[nextStep] = {};
-            }
-            //pointerResult[nextStep] is elementary (string, number, etc.)
-            else {
-                pointerResult[nextStep] = replacer;
-            }
+            pointerResult[nextStep] = replacer;
 
             //We stop going into our JSON
             return;
@@ -46,8 +34,8 @@ ObjectEditor.prototype.editObject = function(replacer) {
 
         //We go to the next step, deleting it from our array, calling auxFindElementToReplace again
         pointerResult = pointerResult[nextStep];
-        arrayOfSteps.shift();
-        auxFindElementToReplace(pointerResult, arrayOfSteps, replacer);
+        array.shift();
+        auxFindElementToReplace(pointerResult, array, replacer);
     })(this.result, arrayOfSteps, replacer);
 };
 
@@ -83,14 +71,12 @@ var breakCyclesInBFS = function(object, customizer) {
     //We instantiate our result root.
     var result = _.isArray(object) ? [] : {};
 
-    //To write the functions in our way
-    var functionRegex = /\([^{]*/;
-
     //We first put all the JSON source in our queues
     queue.push(object);
     queueOfModifiers.push(new ObjectEditor(object, ""));
 
     var positionStack;
+    var nextInsertion;
 
     //BFS algorithm
     while(queue.length > 0) {
@@ -101,74 +87,57 @@ var breakCyclesInBFS = function(object, customizer) {
         //The path that leads to this JSON, so we can build other paths from it
         var path = editor.path;
 
-        //We first make any personalized replacements
+        //We first attempt to make any personalized replacements
         //If customizer doesn't affect the value, customizer(value) returns undefined and we jump this if
         if(customizer !== undefined) {
 
             //By using this variable, customizer(value) is called only once.
             var customizedValue = customizer(value);
 
-            if(customizedValue !== undefined) {
-
-                var newValue = customizedValue;
-
-                //If the value has already been discovered, we fix its circular reference
-                positionStack = _.chain(foundStack)
-                    .pluck("value")
-                    .indexOf(value)
-                    .value();
-                if(positionStack !== -1) {//todo: if(...) newValue = ...; editor.editObject(newValue);
-                    editor.editObject(foundStack[positionStack].makePathName());
-                }
-                else {
-                    editor.editObject(newValue);
-                }
-
-                //The personalized replacements overwrite any other changes
-                continue;//todo: why customizer is separated?
-            }
+            if(customizedValue !== undefined) value = customizedValue;
         }
 
 
         if(typeof value === "object") {
 
-            //If the value has already been discovered, we fix its circular reference and go to the next iteration
             positionStack = _.chain(foundStack)
                 .pluck("value")
                 .indexOf(value)
                 .value();
-            
+
+            //If the value has already been discovered, we only fix its circular reference
             if(positionStack !== -1) {
-                editor.editObject(foundStack[positionStack].makePathName());
-                continue;
+                nextInsertion = foundStack[positionStack].makePathName();
             }
+            else {
+                //At the first time we discover a certain value, we put it in the stack
+                foundStack.push(new FoundObject(value, path));
 
-            //At the first time we discover a certain value, we put it in the stack
-            foundStack.push(new FoundObject(value, path));
+                nextInsertion = value;
 
-            //If the current value is an array, we put "[]" in the result JSON, and all the children in the queues
-            //If value is a nested JSON, we put "{}" in the result JSON, and all children in the queues
-            var type = _.isArray(value) ? "partialArray" : "nestedJSON"; // TODO change these definitions
-            editor.editObject(type);
-
-            for(var component in value) {
-                if(_.has(value, component)) {
-                    queue.push(value[component]);
-                    var newPath = path + "[" + component + "]";
-                    queueOfModifiers.push(new ObjectEditor(result, newPath));
+                for(var component in value) {
+                    if(_.has(value, component)) {
+                        queue.push(value[component]);
+                        var newPath = path + "[" + component + "]";
+                        queueOfModifiers.push(new ObjectEditor(result, newPath));
+                    }
                 }
             }
         }
-        //If it's a function, we only write "f(arg1, arg2, ...) { number of lines }"
-        else if(typeof value === "function") {//todo: why not remove it and let the user put it in customizer?
-            var fs = value.toString();
-            var lineNum = fs.split(/\r\n|\r|\n/).length;
-            editor.editObject("f " + functionRegex.exec(value.toString())[0] + "{ " + lineNum + " }");
-        }
+        ////If it's a function, we only write "f(arg1, arg2, ...) { number of lines }"
+        //else if(typeof value === "function") {//todo: why not remove it and let the user put it in customizer?
+        //    //To write the functions in our way
+        //    var functionRegex = /\([^{]*/;
+        //    var fs = value.toString();
+        //    var lineNum = fs.split(/\r\n|\r|\n/).length;
+        //    editor.editObject("f " + functionRegex.exec(value.toString())[0] + "{ " + lineNum + " }");
+        //}
         //If it's an elementary value, it can't be circular, so we just put this value in our JSON result.
         else {
-            editor.editObject(value);
+            nextInsertion = value;
         }
+
+        editor.editObject(nextInsertion);
     }
 
     return result;
